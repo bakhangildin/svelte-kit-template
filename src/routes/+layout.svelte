f844d9f24982d7cc24e678147a71a70e37c7ca3d
Булат Хангильдин <khangildinba@gmail.com>
2023-09-20 12:39:44 +0300

    added code

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { page } from "$app/stores";
+  import { DarkMode, Navbar, NavLi, NavUl } from "flowbite-svelte";
+  import "../app.css";
+  $: activeUrl = $page.url.pathname;
+</script>
+
+{#key activeUrl}
+  <Navbar let:hidden let:toggle>
+    <NavUl {hidden} {activeUrl}>
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/page">Page</NavLi>
+    </NavUl>
+    <DarkMode />
+  </Navbar>
+{/key}
+
+<slot />
---
+++ src/routes/page/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+</script>
+
+<div>page</div>
+<!-- <NavLi href="/">home</NavLi> -->
